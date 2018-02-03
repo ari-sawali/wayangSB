2bfcbe10a36cb0d6239f5609e14f5ad25e20c1d5
fahri <fahri.petrucci@gmail.com>
2018-02-03 23:50:39 +0700

    Fourth Commit

--- fellyModule/botCfg.py
+++ fellyModule/botCfg.py
@@ -4,20 +4,20 @@ startTime = 0
 cctv = {'cyduk': {}, 'point': {}, 'sidermem': {}, 'haloSider': {}}
 wait = {'botmode': 'self', 'comment': 'Thanks for add me', 'autoCancel': {'on': True, 'members': 1}, 'dblack': False, 'leaveRoom': True, 'cName': '', 'wblack': False, 'autoBattle': False, 'commentBlack': {}, 'dblacklist': False, 'commentOn': False, 'gProtect': True, 'wblacklist': False, 'Protectcancl': True, 'Protectgr': True, 'autoAdd': True, 'timeline': True, 'blacklist': {}, 'autoJoin': True, 'atjointicket': True, 'groupId': '', 'lang': 'JP', 'battleMode': '', 'message': 'Ñ\x82erima Kasih Sudah Menambahkan Aku Jadi Teman', 'clock': False, 'protectionOn': True, 'joinedBoT': '', 'bigAj': {}, 'contact': False, 'contactInvite': False}
 boten = ['ueff8f78401c867593c6ddc8aeb8c649d', 'u9fec89015e171bc9a8f82ce1ded83075', 'ubb4183ea6b5c541817eaa3e6a8c6acfe']
-owner = ['u8eeae85d90ca91ddc2fa6463abde7500']
-admin = ['u8eeae85d90ca91ddc2fa6463abde7500', 'u32677c17bef951259fd924b31a3be8e7', 'u955bb12fc8df1b50a73216028668cb53', 'u299ea38094ecf897b3af1280990fd2b3', 'u9415636d502365256a894ade6b63fe17']
+owner = []
+admin = []
 whitelist = []
-grouplist = ['c39de70718b11214f8234ba8db7c4c31b', 'cfed1f18026bdf0ccd335839f25285533', 'cdf7ff8950aadb5e17fa9d9c063511fa6', 'c863a89c34883f83e924da2f551a7e403']
-groupAdmin = {'c39de70718b11214f8234ba8db7c4c31b': [], 'cfed1f18026bdf0ccd335839f25285533': [], 'cdf7ff8950aadb5e17fa9d9c063511fa6': [], 'c863a89c34883f83e924da2f551a7e403': []}
+grouplist = []
+groupAdmin = {}
 bigGroupId = 'c799a2276413792cd6d00e02188b542ae'
 helpMessage = '\n--◄]·♦·Wayang Dugem·♦·[►--\n-=========================-\n\n-◄]·♦·Menu For Public·♦·[►-\n[•].adminlist\n[•].tagadmin\n[•].cctv/.ciduk\n[•].grinfo\n[•].wl list\n[•].creator\n[•].bot/.woi/.woy\n[•].yt\n\n'
 helpAdmin = '-◄]·♦·Menu For Admin·♦·[►-\n[•].cancel\n[•].oqr (Open QR)\n[•].gurl (Group URL and Open QR)\n[•].curl (Close QR)\n[•].speed/.sp\n[•].status/.set\n[•].summon/.tagall/.mentionall\n[•].absen/.respon\n[•].banlist\n[•].nk\n[•].wl add/remove\n\n'
 helpOwner = '-◄]👑Menu For Owner👑[►-\n[•].protect (on/off)\n[•]Admin add/remove\n[•]grAdmin add/remove\n[•].grAdd\n[•].grRemove\n[•].img\n[•].meme\n[•].spic\n[•].scover\n[•].kuy\n[•].cabut all\n\n'
 helpFooter = '-=========================-\n--◄]·♦·Wayang Dugem·♦·[►--\n-=========================-\n'
 restartVar = {'isRestart': False, 'restartIn': ''}
-protectedGroup = ['c39de70718b11214f8234ba8db7c4c31b']
-autoFind = {'u8eeae85d90ca91ddc2fa6463abde7500': {'herotype': 'legend', 'heroname': 'laguna', 'server': 'ueff8f78401c867593c6ddc8aeb8c649d', 'slot': 1}}
-autoHStore = {'u8eeae85d90ca91ddc2fa6463abde7500': {'herotype': 'legend', 'heroname': 'laguna', 'server': 'u9fec89015e171bc9a8f82ce1ded83075', 'price': '120000', 'slot': 0, 'start': True, 'cooldown': False}}
+protectedGroup = []
+autoFind = {}
+autoHStore = {}
 autoPowerUp = {}
 autoJoinBattle = {'on': False, 'groupId': ''}
 customVar = {}
